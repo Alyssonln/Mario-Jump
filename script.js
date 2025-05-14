17e47aa14f2c943e375fd9ecf611f4594234e92c
Alyssonln <alyssonlnunes@gmail.com>
2025-05-14 15:01:01 -0300

    animação de fundo

--- script.js
+++ script.js
@@ -5,18 +5,22 @@ const jump = () => {
   mario.classList.add('jump')
   setTimeout(() => {
     mario.classList.remove('jump')
-  }, 900)
+  }, 800)
 }
 
 const loop = setInterval(() => {
   const pipePosition = pipe.offsetLeft
   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
-  if (pipePosition <= 125 && pipePosition > 0 && marioPosition < 105) {
+  if (pipePosition <= 180 && pipePosition > 0 && marioPosition < 110) {
     pipe.style.animation = 'none'
     pipe.style.left = `${pipePosition}px`
     mario.style.animation = 'none'
     mario.style.bottom = `${marioPosition}px`
+    mario.src = './img/game-over.png'
+    mario.style.width = '75px'
+    mario.style.marginLeft = '50px'
+    clearInterval(loop)
   }
 }, 10)
 
